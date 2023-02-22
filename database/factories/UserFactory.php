42a66b195b12a518036027acf0e23a3043a1c28e
Dhavalgu <dhavalgondaliya3@gmail.com>
2023-02-22 11:15:32 +0530

    ecs new changes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -21,7 +23,8 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            // password
             'remember_token' => Str::random(10),
         ];
     }
--- ecs.php
+++ ecs.php
@@ -1,23 +1,62 @@
 <?php
 
-declare(strict_types=1);
-
-use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\CodeAnalysis\AssignmentInConditionSniff;
+use PhpCsFixer\Fixer\Basic\BracesFixer;
+use PhpCsFixer\Fixer\Basic\CurlyBracesPositionFixer;
+use PhpCsFixer\Fixer\ClassNotation\ClassDefinitionFixer;
+use PhpCsFixer\Fixer\ClassNotation\SelfAccessorFixer;
+use PhpCsFixer\Fixer\FunctionNotation\NativeFunctionInvocationFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\Operator\ConcatSpaceFixer;
+use PhpCsFixer\Fixer\Operator\IncrementStyleFixer;
+use PhpCsFixer\Fixer\Operator\StandardizeIncrementFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitMethodCasingFixer;
+use PhpCsFixer\Fixer\Semicolon\MultilineWhitespaceBeforeSemicolonsFixer;
+use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
+use Symplify\CodingStandard\Fixer\Spacing\MethodChainingNewlineFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
-
-    $ecsConfig->ruleWithConfiguration(ArraySyntaxFixer::class, [
-        'syntax' => 'short',
+    $ecsConfig->parallel();
+    $ecsConfig->paths([
+        __DIR__.'/app',
+        __DIR__.'/database',
+        __DIR__.'/tests',
     ]);
 
     $ecsConfig->sets([
-        // run and fix, one by one
-        // SetList::SPACES,
-        // SetList::ARRAY,
-        // SetList::DOCBLOCK,
+        SetList::ARRAY,
+        SetList::CLEAN_CODE,
+        SetList::COMMENTS,
+        SetList::COMMON,
+        SetList::CONTROL_STRUCTURES,
+        SetList::DOCBLOCK,
+        SetList::NAMESPACES,
+        SetList::PHPUNIT,
         SetList::PSR_12,
+        SetList::SPACES,
+        SetList::STRICT,
+        SetList::SYMPLIFY,
+    ]);
+
+    $ecsConfig->skip([
+        PhpUnitMethodCasingFixer::class,
+        MethodChainingNewlineFixer::class,
+        MethodChainingIndentationFixer::class,
+        IncrementStyleFixer::class,
+        StandardizeIncrementFixer::class,
+        AssignmentInConditionSniff::class,
+        NativeFunctionInvocationFixer::class,
+        ClassDefinitionFixer::class,
+        ConcatSpaceFixer::class,
+        BracesFixer::class,
+        CurlyBracesPositionFixer::class,
+        SelfAccessorFixer::class,
+    ]);
+
+    $ecsConfig->rule(NoUnusedImportsFixer::class);
+    $ecsConfig->ruleWithConfiguration(MultilineWhitespaceBeforeSemicolonsFixer::class, [
+        'strategy' => 'no_multi_line',
     ]);
 };
